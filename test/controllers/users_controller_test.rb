ce5e4096b0cefb77334e7dd2772d82b14ff458eb
Mykola Kaban <pereslop@gmail.com>
2017-09-12 10:41:04 +0300

    gitignore .idea

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
-#   test "should get new" do
-#     get users_new_url
-#     assert_response :success
-#   end
+  test "should get new" do
+    get new_user_path
+    assert_response :success
+  end
 
 end
