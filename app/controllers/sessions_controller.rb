4cc5f825efec055dc4437ea55efc7735adb46855
Mykola Kaban <pereslop@gmail.com>
2017-09-14 13:59:20 +0300

    addint test for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      #log the user
+    else
+      #create an error message
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  def destroy
+
+  end
 end
