8ee0fa6310816592938e8c0a97210024b4fde927
Mykola Kaban <pereslop@gmail.com>
2017-09-14 15:30:03 +0300

    creating cockies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:sessions][:password])
-      #log the user
+      log_in user
+      redirect_to user
     else
       #create an error message
       flash.now[:danger] = 'Invalid email/password combination'
