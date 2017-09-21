4f732cf1a36e1b5668778633de41693e2747deaf
Mykola Kaban <pereslop@gmail.com>
2017-09-21 19:48:28 +0300

    10.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logget_in_user?, only: [:edit, :update]
+  before_action :correct_user?, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -15,17 +17,50 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
 
   def edit
     @user = User.find_by(params[:id])
   end
+
+  def update
+    @user = resource
+    if @user.update_attributes(user_params)
+      flash[:success] = 'profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+
+
+  def collection
+    User.all
+  end
+
+  def resource
+    collection.find(params[:id])
+  end
+
+  def logget_in_user?
+    unless logged_in?
+      store_location
+      flash[:danger] = 'please log in. '
+      redirect_to login_url
+    end
+  end
+
+  def correct_user?
+    redirect_to(root_url) unless current_user?(resource)
   end
 end
