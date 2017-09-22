60418a5d4e4941a8ab98670bbf3de88f3bd66b52
Mykola Kaban <pereslop@gmail.com>
2017-09-22 10:33:10 +0300

    added pagination for users/index template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   before_action :logget_in_user?, only: [:index, :edit, :update]
   before_action :correct_user?, only: [:edit, :update]
+
   def index
-    @users = collection
+    @users = User.paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
   end
